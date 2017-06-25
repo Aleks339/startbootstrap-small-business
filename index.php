f13f9aae498b79a361f10bf9006432c9253afc65
Vasilii Chugunov <naukomania@gmail.com>
2017-06-25 14:45:30 +0300

    add phrases

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $l = $data[$lang];
             <div class="col-md-4">
                 <h1><?= $l["top-header"] ?></h1>
                 <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-                <a class="btn btn-primary btn-lg" href="#">Call to Action!</a>
+                <a class="btn btn-primary btn-lg" href="#"><?= $l["call-to-action"] ?></a>
             </div>
             <!-- /.col-md-4 -->
         </div>
--- protected/data.php
+++ protected/data.php
@@ -1,15 +1,17 @@
 <?
 //ru
 $ru = array();
-$ru ["top-header"] = "Анна Муляр";
-
+$ru["top-header"] = "Анна Муляр";
+$ru["call-to-action"] = "Свяжитесь со мной";
 //zh
 $zh = array();
-$zh ["top-header"] = "Ni hao";
+$zh["top-header"] = "Ni hao";
+$zh["call-to-action"] = "Wo shi e guo ren";
 
 //en
 $en = array();
-$en ["top-header"] = "Anja Muljar";
+$en["top-header"] = "Anja Muljar";
+$en["call-to-action"] = "Send to me";
 
 //data
 $data = array(
